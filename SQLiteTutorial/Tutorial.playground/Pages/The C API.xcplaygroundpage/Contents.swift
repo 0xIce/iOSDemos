b9100ebf314f5eb9ec4884abf3d46dcae95a5643
hongqima <hongqima@yangqianguan.com>
2019-06-27 14:56:24 +0800

    add c api

--- SQLiteTutorial/Tutorial.playground/Pages/The C API.xcplaygroundpage/Contents.swift
+++ SQLiteTutorial/Tutorial.playground/Pages/The C API.xcplaygroundpage/Contents.swift
@@ -45,22 +45,211 @@ destroyPart1Database()
  `mkdir -p ~/Documents/Shared\ Playground\ Data/SQLiteTutorial`
  
  */
-
 //: ## Open a Connection
+func openDatabase() -> OpaquePointer? {
+  var db: OpaquePointer?
+  if sqlite3_open(part1DbPath, &db) == SQLITE_OK {
+    print("Successfully opened connection to database at \(part1DbPath)")
+    return db
+  } else {
+    print("Unable to open database. Verify that you created the directory described " +
+      "in the Getting Started section.")
+    PlaygroundPage.current.finishExecution()
+  }
+}
 
+let db = openDatabase()
 //: ## Create a Table
+let createTableString = """
+CREATE TABLE Contact(
+Id INT PRIMARY KEY NOT NULL,
+Name CHAR(255));
+"""
+func createTable() {
+  var createTableStatement: OpaquePointer?
+  if sqlite3_prepare_v2(db, createTableString, -1, &createTableStatement, nil) == SQLITE_OK {
+    if sqlite3_step(createTableStatement) == SQLITE_DONE {
+      print("Contact table created.")
+    } else {
+      print("Contact table could not be created.")
+    }
+  } else {
+    print("CREATE TABLE statement could not be prepared.")
+  }
+  
+  sqlite3_finalize(createTableStatement)
+}
 
+createTable()
 //: ## Insert a Contact
+let insertStatementString = "INSERT INTO Contact (Id, Name) VALUES (?, ?);"
 
+func insert() {
+  var insertStatement: OpaquePointer?
+  
+  if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+    let id: Int32 = 1
+    let name: NSString = "Ray"
+    
+    sqlite3_bind_int(insertStatement, 1, id)
+    sqlite3_bind_text(insertStatement, 2, name.utf8String, -1, nil)
+    
+    if sqlite3_step(insertStatement) == SQLITE_DONE {
+      print("Successfully inserted row.")
+    } else {
+      print("Could not insert row.")
+    }
+  } else {
+    print("INSERT statement could not be prepared.")
+  }
+  
+  sqlite3_finalize(insertStatement)
+}
+
+insert()
+
+//: ## Multi Insert
+func multiInsert() {
+  
+  var insertStatement: OpaquePointer? = nil
+  // 1
+  let names: [NSString] = ["Ray", "Chris", "Martha", "Danielle"]
+  
+  if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+    
+    // 2
+    for (index, name) in names.enumerated() {
+      // 3
+      let id = Int32(index + 1)
+      sqlite3_bind_int(insertStatement, 1, id)
+      sqlite3_bind_text(insertStatement, 2, name.utf8String, -1, nil)
+      
+      if sqlite3_step(insertStatement) == SQLITE_DONE {
+        print("Successfully inserted row.")
+      } else {
+        print("Could not insert row.")
+      }
+      // 4
+      sqlite3_reset(insertStatement)
+    }
+    
+    sqlite3_finalize(insertStatement)
+  } else {
+    print("INSERT statement could not be prepared.")
+  }
+}
+
+multiInsert()
 //: ## Querying
+let queryStatementString = "SELECT * FROM Contact;"
+func query() {
+  var queryStatement: OpaquePointer?
+  
+  if sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+    if sqlite3_step(queryStatement) == SQLITE_ROW {
+      let id = sqlite3_column_int(queryStatement, 0)
+      
+      let queryResultCol1 = sqlite3_column_text(queryStatement, 1)
+      let name = String(cString: queryResultCol1!)
+      
+      print("Query Result:")
+      print("\(id) | \(name)")
+    } else {
+      print("Query returned no results")
+    }
+  } else {
+    print("SELECT statement could not be prepared")
+  }
+  
+  sqlite3_finalize(queryStatement)
+}
 
+query()
+
+//: ## Query All
+func queryAll() {
+  var queryStatement: OpaquePointer? = nil
+  if sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+    
+    while (sqlite3_step(queryStatement) == SQLITE_ROW) {
+      let id = sqlite3_column_int(queryStatement, 0)
+      let queryResultCol1 = sqlite3_column_text(queryStatement, 1)
+      let name = String(cString: queryResultCol1!)
+      print("Query Result:")
+      print("\(id) | \(name)")
+    }
+    
+  } else {
+    print("SELECT statement could not be prepared")
+  }
+  sqlite3_finalize(queryStatement)
+}
+
+//queryAll()
 //: ## Update
+let updateStatementString = "UPDATE Contact SET Name = 'Chris' WHERE Id = 1;"
+func update() {
+  var updateStatement: OpaquePointer? = nil
+  if sqlite3_prepare_v2(db, updateStatementString, -1, &updateStatement, nil) == SQLITE_OK {
+    if sqlite3_step(updateStatement) == SQLITE_DONE {
+      print("Successfully updated row.")
+    } else {
+      print("Could not update row.")
+    }
+  } else {
+    print("UPDATE statement could not be prepared")
+  }
+  sqlite3_finalize(updateStatement)
+}
 
+update()
+query()
 //: ## Delete
+let deleteStatementStirng = "DELETE FROM Contact WHERE Id = 3;"
 
+func delete() {
+  var deleteStatement: OpaquePointer? = nil
+  if sqlite3_prepare_v2(db, deleteStatementStirng, -1, &deleteStatement, nil) == SQLITE_OK {
+    if sqlite3_step(deleteStatement) == SQLITE_DONE {
+      print("Successfully deleted row.")
+    } else {
+      print("Could not delete row.")
+    }
+  } else {
+    print("DELETE statement could not be prepared")
+  }
+  
+  sqlite3_finalize(deleteStatement)
+}
+
+delete()
+queryAll()
 //: ## Errors
+let malformedQueryString = "SELECT Stuff from Things WHERE Whatever;"
+func prepareMalformedQuery() {
+  var malformedStatement: OpaquePointer? = nil
+  
+  if sqlite3_prepare_v2(db, malformedQueryString, -1, &malformedStatement, nil) == SQLITE_OK {
+    print("This should not have happened.")
+  } else {
+    let errorMessage = String.init(cString: sqlite3_errmsg(db))
+    print("Query could not be prepared! \(errorMessage)")
+  }
+  
+  sqlite3_finalize(malformedStatement)
+}
 
+prepareMalformedQuery()
 //: ## Close the database connection
-
+func close() {
+  if sqlite3_close(db) == SQLITE_OK {
+    print("database connection closed successfully")
+  } else {
+    let errorMessage = String.init(cString: sqlite3_errmsg(db))
+    print("database connection close errod: \(errorMessage)")
+  }
+}
+close()
+close()
 //: Continue to [Making It Swift](@next)
 
