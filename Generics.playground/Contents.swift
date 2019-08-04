1a064a499c08bd9d526cbd246f914740af28e264
hongqima <hongqima@yangqianguan.com>
2019-08-04 11:47:36 +0800

    generic

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,8 @@
+
+let countryCodes = ["Arendelle": "AR", "Genovia": "GN", "Freedonia": "FD"]
+let countryCode = countryCodes["Freedonia"]
+
+let optionalName = Optional<String>.some("Princess Moana")
+if let name = optionalName {}
+
+Result<Any, <#Failure: Error#>>
